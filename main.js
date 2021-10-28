5e8e76ca9433f070a4b2a8e7f98ba77fa20bb714
avanisrivastava <78412779+avanisrivastava@users.noreply.github.com>
2021-10-28 12:09:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+   canvas=createCanvas(400,400);
+canvas.position(600,240)
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save('lipstick_filter.png');
+}
